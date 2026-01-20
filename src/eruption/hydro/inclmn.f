8afa94f677ecfff7c0b4ae2f1c509b836ab61f85
DTsuna <dt.fermat125@gmail.com>
2026-01-20 09:49:56 +0900

    update .gitignore file

--- src/eruption/hydro/inclmn.f
+++
@@ -1,2 +0,0 @@
-      integer mn, nelem, nrow, ncol
-      parameter ( mn = 10010, nelem = 19, nrow = 107, ncol = 19 )
--- src/eruption/hydro/para1.d
+++
@@ -1,9 +0,0 @@
-ISTART, IHYDM,  WINDOW, INTERVAL TO LOOK, idev
-0    9999999      10        10000          11
-MASS CUT    COURNT NUMBER   EEXPL       Nenv
-0.0           0.4D0      0     0
-TIME TO OUTPUT 2.3E5, 3E5, 4.32E5,
-4, 1.d12, 1.d13, 1.d14, 1.d15
-FILE NAMES TO OUTPUT
-snhydOutput/hyd.d
-snhydOutput/lc.d
